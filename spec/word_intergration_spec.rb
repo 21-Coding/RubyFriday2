3eefe7b39bbaab03e68314454c18bc5ec0018fd2
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-20 13:40:06 -0800

    add wireframing for next test and passing active save method as opposed to bones of save method

---
+++ spec/word_intergration_spec.rb
@@ -0,0 +1,35 @@
+require('rspec')
+require('word.rb')
+
+describe '#Word' do
+
+  before(:each) do
+    Word.clear()
+  end
+
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all words from list") do
+      word = Word.new("Share", nil)
+      word.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
+  # 
+  # describe('') do
+  #   it("clears all words from list") do
+  #     word = Word.new("Share", nil)
+  #     word.save()
+  #     Word.clear()
+  #     expect(Word.all).to(eq([]))
+  #   end
+  # end
+
+
+end
