163dac731ffa0a3119e29e16d0d9de8c556e6464
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-20 09:41:48 -0800

    add wireframing for word.rb

---
+++ lib/word.rb
@@ -0,0 +1,26 @@
+class Word
+  attr_accessor :word, :id
+  @@word = {}
+
+  def initialize(input, id)
+  @input = input
+  @id = id
+  end
+
+  def save
+
+  end
+  
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  def edit
+
+  end
+
+end
