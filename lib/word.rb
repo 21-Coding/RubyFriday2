3b1ef7a89783a91364e4822052fb1c3a3b96e5cb
DominiqueYoumans <ddyoumans@icloud.com>
2020-01-12 21:46:25 -0800

    add spacing to methods for easier readability

--- lib/word.rb
+++ lib/word.rb
@@ -4,33 +4,45 @@ class Word
   @@total_rows = 0
 
   def initialize(word, id)
-  @word = word
-  @id = id || @@total_rows += 1
+    @word = word
+    @id = id || @@total_rows += 1
   end
+
+
+
   def self.clear
     @@words = {}
     @@total_rows = 0
 
   end
 
+
+
+
   def ==(matching_word)
     self.word() == matching_word.word()
   end
+
+
 
   def self.all()
     @@words.values()
 
   end
-#
+
+
+
   def save
     @@words[self.id] = Word.new(self.word, self.id)
   end
+
 
 
   def self.find_word(id)
     @@words[id]
 
   end
+
 
 
   def self.all
@@ -45,18 +57,20 @@ class Word
 
   end
 
+
   def save
     @@words[self.id] = Word.new(self.word, self.id)
   end
+
 
   def delete
     @@words.delete(self.id)
   end
 
-def update(word)
-  self.word = word
-  @@words[self.id] = Word.new(self.word, self.id)
-end
+  def update(word)
+    self.word = word
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
 
 
 
