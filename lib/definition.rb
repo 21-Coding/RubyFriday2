d1477ff9b241787a962460c8e0e90fb6997981ac
DominiqueYoumans <ddyoumans@icloud.com>
2020-01-12 21:46:50 -0800

    add 'all' method

---
+++ lib/definition.rb
@@ -0,0 +1,26 @@
+class Definition
+
+  attr_reader :id
+  attr_accessor :text, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(text, word_id, id)
+    @text = text
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@definitions.values
+  end
+
+
+
+  def self.clear
+    @@definitions = {}
+    @@total_rows = 0
+
+  end
+end
