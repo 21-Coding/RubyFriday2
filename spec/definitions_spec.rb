5298abb885419401557cf7b31aaeb0e3f177f282
DominiqueYoumans <ddyoumans@icloud.com>
2020-01-15 19:28:45 -0800

    add passing spec

--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -15,16 +15,25 @@ describe '#Definition' do
 
   describe('.save') do
     it "saves a definition" do
-      definition = Definition.new("Share", 1, nil)
+      definition = Definition.new("Share(v): have a portion of (something) with another or others.", 1, nil)
       definition.save()
       expect(Definition.all).to(eq([definition]))
+    end
+  end
+
+  describe('#update') do
+    it "updates a definition by id" do
+      definition = Definition.new("Share(v): have a portion of (something) with another or others.", nil, nil)
+      definition.save()
+      definition.update("Share(n): a part or portion of a larger amount which is divided among a number of people, or to which a number of people contribute.", nil)
+      expect(definition.text).to(eq("Share(n): a part or portion of a larger amount which is divided among a number of people, or to which a number of people contribute."))
     end
   end
 
 
   describe('.clear') do
     it("clears all definitions from list.") do
-      definition = Definition.new("Share", nil, nil)
+      definition = Definition.new("Share(v): have a portion of (something) with another or others.", nil, nil)
       definition.save()
       Definition.clear()
       expect(Definition.all).to(eq([]))
@@ -33,8 +42,8 @@ describe '#Definition' do
 
   describe('#==') do
     it "is the same definition if attributes are matching another definition" do
-      definition = Definition.new("Share", nil, nil)
-      definition2 = Definition.new("Share", nil, nil)
+      definition = Definition.new("Share(v): have a portion of (something) with another or others.", nil, nil)
+      definition2 = Definition.new("Share(v): have a portion of (something) with another or others.", nil, nil)
       expect(definition).to(eq(definition2))
     end
   end
