a1a4ea98756d4f3c4b6505e311adc07c56d94ff4
DominiqueYoumans <ddyoumans@icloud.com>
2020-01-15 21:22:17 -0800

    add all routes for each class

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ get('/') do
 end
 
 get('/words') do
-
+  @words = Word.all
+  erb(:words)
 end
 
 get('/words/new') do
@@ -20,8 +21,63 @@ end
 
 post('/words') do
   word_name = params[:word_name]
-  word = Word.new(name, nil)
+  word = Word.new(word, nil)
   word.save()
   @words = Word.all()
   erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:text])
+  @words = Word.all
+  erb(:words)
+end
+
+patch('/words/:id')do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:text])
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+
+end
+
+post('/words/:id/definitions')do
+  @word = Word.find(params[:definition_id].to_i())
+  definition = Definition.new(params[:input_def], nil, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:text], nil)
+  erb(:word)
+
+end
+
+delete('/words/:id/definition/:definition_id')do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+
 end
